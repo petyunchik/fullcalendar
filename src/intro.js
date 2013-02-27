c0405fdad94406f3e85ea34cc2b96e92abe68c06
petyunchik <ipetrix@gmail.com>
2013-02-27 15:09:19 +0400

    Original fullcalendar updated to 1.5.4 version

--- src/intro.js
+++ src/intro.js
@@ -1,19 +1,14 @@
-/**
- * @preserve
- * FullCalendar ResourceViews version @VERSION
- * http://tux.fi/~jarnok/fullcalendar-resourceviews/
- * http://arshaw.com/fullcalendar/
+/*!
+ * <%= meta.title %> v<%= meta.version %>
+ * <%= meta.homepage %>
  *
  * Use fullcalendar.css for basic styling.
  * For event drag & drop, requires jQuery UI draggable.
  * For event resizing, requires jQuery UI resizable.
  *
- * Copyright (c) 2012 Jarno Kurlin
- * Copyright (c) 2011 Adam Shaw
+ * (c) <%= meta.copyright %>
  * Dual licensed under the MIT and GPL licenses, located in
  * MIT-LICENSE.txt and GPL-LICENSE.txt respectively.
- *
- * Date: @DATE
  *
  */
  
